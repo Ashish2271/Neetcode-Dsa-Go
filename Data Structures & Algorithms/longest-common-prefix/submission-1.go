f145a8b6e4955551796916b0d262dc4c915f1369
Ashish Pandey <97328833+Ashish2271@users.noreply.github.com>
2026-04-10 07:34:53 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.go
@@ -0,0 +1,14 @@
+func longestCommonPrefix(strs []string) string {
+    prefix := ""
+   for i:=0; i<len(strs[0]); i++ {
+    for j:=1; j<len(strs); j++ {
+
+      if i>=len(strs[j]) || strs[j][i] != strs[0][i]{
+        return prefix
+      }
+     
+    }
+     prefix += string(strs[0][i])
+   }
+   return prefix 
+}
